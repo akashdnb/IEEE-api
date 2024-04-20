79894d1514ea1fb2abaedabe1de17a7b70bb2da7
sulochan <sulochankhadka25@gmail.com>
2024-04-20 19:31:02 +0530

    added-dummy-api

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,8 +2,20 @@ const express = require('express');
 const router = express.Router();
 const eventController = require('../controllers/eventsController');
 
-
 router.get('/events', eventController.getEvents);
 router.get('/events/:id', eventController.getEventById);
+router.get('/dummy', (req, res) => {
+  setInterval(() => {
+    fetch('https://eager-twill-ray.cyclic.app/api/events')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => console.log('Not sleeping'))
+      .catch((error) => console.error('Error:', error));
+  }, 3600000);
+});
 
 module.exports = router;
